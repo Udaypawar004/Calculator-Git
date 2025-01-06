9b27d8b572c5de4ee52431df13866425e5bb98df
Uday Deshmukh <udaypawar004@gmail.com>
2025-01-06 22:16:13 +0530

    Added Subtraction

--- Calculator.java
+++ Calculator.java
@@ -8,14 +8,28 @@ public class Calculator{
 		double num1 = sc.nextDouble();
 		System.out.print("Enter second number: ");
 		double num2 = sc.nextDouble();
-		
-		double result = addition(num1, num2);
-		System.out.println("The addition is: " + result);
-		
+
+		System.out.println("Choose an option");
+		System.out.println("1. Addition");
+		System.out.println("2. Subtraction");
+		double result;
+		int option = sc.nextInt();
+		switch(option){
+			case 1: result=addition(num1, num2);
+			        System.out.println("The addition is:" + result);
+			        break;
+			case 2: result= subtraction(num1, num2);
+			        System.out.println("The Subtraction is: " + result);
+			        break;
+			default: System.out.println("Invalid option.");
+		}
 		sc.close();
 	}
 
 	public static double addition(double num1, double num2) {
 		return num1 + num2;
+	}
+	public static double subtraction(double num1, double num2) {
+		return num1 - num2;
 	}
 }
